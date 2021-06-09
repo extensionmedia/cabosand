b98288525187293f9508622d565cd62d7aab10f1
Desktop <elmeftouhi@extensionmedia.ma>
2021-06-09 23:26:56 +0200

    first commit

---
+++ templates/global/footer.php
@@ -0,0 +1,23 @@
+<script src="<?= HTTP.HOST ?>templates/global/js/api/jquery-3.5.0.min.js"></script>
+<script src="<?= HTTP.HOST ?>templates/global/js/api/jquery.table2excel.min.js"></script>
+
+<script src="<?= HTTP.HOST ?>templates/global/js/api/Chart.min.js"></script>
+<script src="<?= HTTP.HOST ?>templates/global/js/api/sweetalert2.min.js"></script>
+<script src="<?= HTTP.HOST ?>templates/global/js/api/moment.min.js"></script>
+<script src="<?= HTTP.HOST ?>templates/global/js/api/moment.min.fr.js"></script>
+<script src="<?= HTTP.HOST ?>templates/global/js/api/Yjs.js"></script>
+<script src="<?= HTTP.HOST ?>templates/global/js/app.js"></script>
+
+<script src="<?= HTTP.HOST ?>templates/<?= APP_TEMPLATE ?>/js/load.js?version=<?= time() ?>"></script>
+<script src="<?= HTTP.HOST ?>templates/<?= APP_TEMPLATE ?>/js/app.js?version=<?= time() ?>"></script>
+<script src="<?= HTTP.HOST ?>templates/<?= APP_TEMPLATE ?>/js/list.js?version=<?= time() ?>"></script>
+<script src="<?= HTTP.HOST ?>templates/<?= APP_TEMPLATE ?>/js/manager.js?version=<?= time() ?>"></script>
+
+<script src="<?= HTTP.HOST ?>templates/<?= APP_TEMPLATE ?>/js/support.js?version=<?= time() ?>"></script>
+<script src="<?= HTTP.HOST ?>templates/<?= APP_TEMPLATE ?>/js/calendar.js?version=<?= time() ?>"></script>
+<!--
+<script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+-->
+
+</body>
+</html>
