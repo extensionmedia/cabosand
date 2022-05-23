f5135d81f395c97a0f1b93627eadd7f4dec0fcc5
PC <elmeftouhi@extensionmedia.ma>
2022-05-23 21:51:38 +0100

    couneter of periode

--- app/Core/Propriete.php
+++ app/Core/Propriete.php
@@ -281,7 +281,7 @@ class Propriete extends Modal{
 
 				if($year != ""){
 
-					$contrat = $this->find('', array("conditions AND"=>array("YEAR(de)="=>$year, "id_propriete="=>$v["id"])), "v_propriete_proprietaire_location");
+					$contrat = $this->find('', array("conditions AND"=>array("YEAR(de)="=>$year, "id_propriete="=>$v["id"], "status="=>1)), "v_propriete_proprietaire_location");
 
 					$status = $this->Get_Status_Of_Propriete(['year'=>$year, 'id_propriete'=>$v['id']]);
 					$background = $status? $status["hex_string"]: "";
